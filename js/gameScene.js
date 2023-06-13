d4f49c191733db91332153d2d0941e9c13ad1b59
Messi-Sui <124379820+Messi-Sui@users.noreply.github.com>
2023-06-13 22:20:25 +0000

    Working code with defenders generated every 2 seconds

--- js/gameScene.js
+++ js/gameScene.js
@@ -57,35 +57,53 @@ class GameScene extends Phaser.Scene {
     this.load.audio('music', './assets/brazilSong.wav')
   }
 
-  create (data) {
-
-    // Soundtrack
-    const song = this.sound.add('music');
+  create(data) {
+  // Soundtrack
+  const song = this.sound.add('music');
   song.loop = true;
   song.play();
-    
-    this.background = this.add.image(0, 0, 'soccerBackground').setScale(2.75)
-    this.background.setOrigin(0, 0)
 
-    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+  this.background = this.add.image(0, 0, 'soccerBackground').setScale(2.75);
+  this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'soccerPlayer')
+  this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle);
 
-      // Create the Game Over sound
+  this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'soccerPlayer');
+
+  // Create the Game Over sound
   const gameOverSound = this.sound.add('overSound');
 
-    // create a group for the soccer ball
-    this.missileGroup = this.physics.add.group()
+  // create a group for the soccer ball
+  this.missileGroup = this.physics.add.group();
 
-    // create a group for the defender
-    this.alienGroup = this.add.group()
-    this.createAlien()
-    this.createAlien()
-    this.createAlien()
-    this.createAlien()
-    this.createAlien()
+  // create a group for the defender
+  this.alienGroup = this.add.group();
+  this.createAlien()
+    
 
-    //collisions between ball and defender
+  // Function to create an alien
+  const createAlien = () => {
+    const alienXLocation = Math.floor(Math.random() * 1920) * 1;
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1;
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1;
+
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'defender');
+    anAlien.body.velocity.y = 200;
+    anAlien.body.velocity.x = alienXVelocity;
+    anAlien.setScale(0.35);
+
+    this.alienGroup.add(anAlien);
+  };
+
+  // Create a timer event to call createAlien every 2 seconds
+  const alienTimer = this.time.addEvent({
+    delay: 2000,
+    callback: createAlien,
+    callbackScope: this,
+    loop: true
+  });
+
+  //collisions between ball and defender
     this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
       alienCollide.destroy()
       missileCollide.destroy()
@@ -96,25 +114,23 @@ class GameScene extends Phaser.Scene {
       this.createAlien()
     }.bind(this))
 
-      // Collisions between ship and aliens
-      this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
-      gameOverSound.play();
-      song.pause('music')
-      this.physics.pause()
-      alienCollide.destroy()
-      shipCollide.destroy()
-      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
-      this.gameOverText.setInteractive({ useHandCursor: true })
-      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
-      this.score = 0
-        if (song.isPlaying) {
-          gameOverSound.pause('overSound')
-        }
-  }.bind(this))
-    
-    
+  // Collisions between ship and aliens
+  this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+    gameOverSound.play();
+    song.pause('music');
+    this.physics.pause();
+    alienCollide.destroy();
+    shipCollide.destroy();
+    this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5);
+    this.gameOverText.setInteractive({ useHandCursor: true });
+    this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'));
+    this.score = 0;
+    if (song.isPlaying) {
+      gameOverSound.pause('overSound');
+    }
+  }.bind(this));
+
 }
-    
 
     update (time, delta) {
     // called 60 times a second, hopefully!
@@ -202,11 +218,11 @@ class GameScene extends Phaser.Scene {
       if (item.y < 50) {
         item.destroy()
       }
-    })
+    })  
   }
 }
 
 
-export default GameScene
+export default GameScene;
 
     
